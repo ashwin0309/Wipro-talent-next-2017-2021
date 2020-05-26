6eec306f1b200908e9803f04f6255f8cb38edea9
ashwin0309 <65951031+ashwin0309@users.noreply.github.com>
2020-05-26 16:34:41 +0530

    Create Sumofevenodd.java

---
+++ Sumofevenodd.java
@@ -0,0 +1,27 @@
+package sumevenodd;
+
+import java.util.Scanner;
+
+public class Sumofevenodd {
+	
+	public static void main(String[] args)
+	{
+		int a,b;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the value of a: ");
+		a =s.nextInt();
+		Scanner t = new Scanner(System.in);
+		System.out.println("Enter the value of b: ");
+		b =t.nextInt();
+		if((a+b)%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+			
+	}
+
+}
